8e605fa25ddfc49ba08addb657d1ee8d6c2e73cf
Pedro Ferreira <pedroocferreira@gmail.com>
2018-12-24 03:21:03 -0200

    exemplo api google map

---
+++ README.md
@@ -0,0 +1,12 @@
+##This is a quick example of Ionic 3 app which use
+##Geolocation
+##GoogleMaps
+##Native Geolocoder
+
+Nothing fancy in it
+
+[GeoLocation Plugin] (https://ionicframework.com/docs/native/geolocation/)
+
+[GoogleMap plugin] (https://ionicframework.com/docs/native/google-maps/)
+
+[Native Geocoder Plugin] (https://ionicframework.com/docs/native/native-geocoder/)
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { Geolocation } from '@ionic-native/geolocation';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ GoogleMapOptions,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
+import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
+
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    GoogleMaps,
+    NativeGeocoder,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
